fa185cab55487578345bf41d35163f4e545aa925
Kevin Muriuki Karimi <kevimuri24@gmail.com>
2024-10-09 10:38:32 -0400

    Finished Lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,25 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        
+    @property
+    def word(self):
+        return self._word
+    
+    @word.setter
+    def word(self, value):
+        if value.isalpha():
+            self._word = value.lower()
+        else:
+            raise ValueError("Word must contain only alphabetic characters.")
+        
+    def match(self, words):
+        return [word for word in words if sorted(self.word) == sorted(word.lower())]
+        
+
+    
+
+listen = Anagram("listen")
+print(listen.match(['enlists', 'google', 'inlets', 'banana']))
